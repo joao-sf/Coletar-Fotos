f0382fecead43f69a3c86564243f181aa5c60c59
joao-sf <silva.joao.filho@gmail.com>
2023-06-08 13:26:29 -0300

    Pré compilação

--- main.py
+++ main.py
@@ -5,16 +5,21 @@ import pandas as pd
 # Obter o caminho absoluto do diretório atual
 diretorio_atual = os.path.dirname(os.path.abspath(__file__))
 
-# Construir o caminho completo da planilha
-caminho_planilha = os.path.join(diretorio_atual, 'Lote.xlsx')
+# Construir o caminho completo da pasta "Dados" e do arquivo CSV
+pasta_dados = os.path.join(diretorio_atual, 'Dados')
+caminho_planilha = os.path.join(pasta_dados, 'dados.csv')
 
-# Ler a planilha e obter os dados da primeira coluna
-planilha = pd.read_excel(caminho_planilha)
+# Ler a planilha (arquivo CSV) e obter os dados da primeira coluna
+planilha = pd.read_csv(caminho_planilha)
 primeira_coluna = planilha.iloc[:, 0]
 
 # Diretórios de origem e destino dos arquivos
-diretorio_origem = os.path.join(diretorio_atual, 'fotos')
-diretorio_destino = diretorio_atual
+diretorio_origem = os.path.join(diretorio_atual, 'imagens')
+diretorio_destino = os.path.join(diretorio_atual, 'Encontrados')
+
+# Criar a pasta "Encontrados" se não existir
+if not os.path.exists(diretorio_destino):
+    os.makedirs(diretorio_destino)
 
 # Adicionar coluna de status na planilha
 planilha['Status'] = ''
@@ -23,7 +28,6 @@ planilha['Status'] = ''
 for i, valor in enumerate(primeira_coluna):
     # Converter o valor para string
     valor_str = str(valor)
-
 
     # Obter o nome do arquivo correspondente ao valor
     nome_arquivo = next((arquivo for arquivo in os.listdir(diretorio_origem) if arquivo.startswith(valor_str)), None)
@@ -44,6 +48,6 @@ for i, valor in enumerate(primeira_coluna):
         # Atualizar o status na planilha
         planilha.at[i, 'Status'] = 'Não encontrado'
 
-# Salvar a planilha atualizada
-caminho_planilha_atualizada = os.path.join(diretorio_atual, 'Lote_atualizada.xlsx')
+# Salvar a planilha atualizada na pasta "Dados"
+caminho_planilha_atualizada = os.path.join(pasta_dados, 'Dados_atualizada.xlsx')
 planilha.to_excel(caminho_planilha_atualizada, index=False)
